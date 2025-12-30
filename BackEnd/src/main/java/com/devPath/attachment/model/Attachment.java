50437ec53cc535b08e6ad1546bfe192ee98db54d
Meryem Berkaoui <meryem.berkaoui20@meryem.com>
2025-12-30 16:48:09 +0100

    Create Class Resouce & Attachment (ps : just the classes)

---
+++ BackEnd/src/main/java/com/devPath/attachment/model/Attachment.java
@@ -0,0 +1,26 @@
+package com.devPath.attachment.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "attachments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Attachment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fileName;
+
+    private String fileType;
+
+    private String fileURL;
+
+
+}
---
+++ BackEnd/src/main/java/com/devPath/shared/enums/ResourceType.java
@@ -0,0 +1,7 @@
+package com.devPath.shared.enums;
+
+public enum ResourceType {
+    CV,
+    FAQ,
+    INTERVIEW
+}
